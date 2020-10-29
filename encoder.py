ad48b66d223be16a2ee6779a509516dc13fb5256
Steven Maio <stevenmaio.321@gmail.com>
2020-10-29 11:35:09 -0400

    Added supported for custom weights in truncation of DCT terms

--- encoder.py
+++ encoder.py
@@ -10,7 +10,7 @@ import numpy as np
 from PIL import Image
 from itertools import product, zip_longest
 
-def encode(fp, output):
+def encode(fp, output, weights=WEIGHTS_DEFAULT):
     '''
     Encodes the file whose name is given by fp into the appropriate format.
     The image must be in RGB format.
@@ -35,7 +35,7 @@ def encode(fp, output):
         cb_matrix = np.zeros((DIMENSIONS,DIMENSIONS))
         cr_matrix = np.zeros((DIMENSIONS,DIMENSIONS))
         # iterate through each 8x8 of the image
-        for step_i,step_j in product(range(height_steps), range(width_steps)):
+        for step_i,step_j in product(range(width_steps), range(height_steps)):
             row_base = DIMENSIONS*step_i
             col_base = DIMENSIONS*step_j
             # convert the pixels to YCbCr format
@@ -52,9 +52,7 @@ def encode(fp, output):
             transformed_y = quantize(transformed_y, QUANT_DEFAULT)
             transformed_cb = quantize(transformed_cb, QUANT_DEFAULT)
             transformed_cr = quantize(transformed_cr, QUANT_DEFAULT)
-            # need to figure out a good way to undo this -- or it won't be
-            # a problem?
-            indices = filter(lambda x: min(x[0],x[1]) <= DIMENSIONS//2,
+            indices = filter(lambda x: weights[x] != 0,
                              product(range(DIMENSIONS),repeat=2))
             for i,j in indices:
                 y = int(transformed_y[i,j]).to_bytes(1,'little',signed=True)
@@ -65,7 +63,7 @@ def encode(fp, output):
                 outfile.write(cr)
 
 
-def decode(fp, output):
+def decode(fp, output, weights=WEIGHTS_DEFAULT):
     '''
     Encodes the file whose name is given by fp into the appropriate format.
     The image must be in RGB format.
@@ -77,6 +75,7 @@ def decode(fp, output):
             the path of the output file
     '''
     with open(fp, "rb") as data:
+        # FIXME: These I/O operations can be done so much better
         width_steps = int.from_bytes(data.read(1),'little')
         height_steps = int.from_bytes(data.read(1),'little')
         im = Image.new('RGB', (width_steps*DIMENSIONS, height_steps*DIMENSIONS))
@@ -86,12 +85,13 @@ def decode(fp, output):
         transformed_cb = np.zeros((DIMENSIONS, DIMENSIONS))
         transformed_cr = np.zeros((DIMENSIONS, DIMENSIONS))
         # iterate through 8x8 block
-        for step_i,step_j in product(range(height_steps),range(width_steps)):
-            i_base = DIMENSIONS*step_i
-            j_base = DIMENSIONS*step_j
-            indices = filter(lambda x: min(x[0],x[1]) <= DIMENSIONS//2,
+        for step_i,step_j in product(range(width_steps),range(height_steps)):
+            col_base = DIMENSIONS*step_i
+            row_base = DIMENSIONS*step_j
+            indices = filter(lambda x: weights[x] != 0,
                              product(range(DIMENSIONS),repeat=2))
             for pos in indices:
+                # FIXME: These I/O operations can be done so much better
                 y = int.from_bytes(data.read(1),'little',signed=True)
                 cb = int.from_bytes(data.read(1),'little',signed=True)
                 cr = int.from_bytes(data.read(1),'little',signed=True)
@@ -110,18 +110,14 @@ def decode(fp, output):
                 y = recovered_y[i,j]
                 cb = recovered_cb[i,j]
                 cr = recovered_cr[i,j]
-                r,g,b = YCbCr_to_RGB((y,cb,cr))
-                r = int(r)
-                g = int(g)
-                b = int(b)
-                pos = (i_base+i, j_base+j)
-                val = (r,g,b)
-                im.putpixel(pos, val)
+                rgb = tuple(map(lambda t : int(t), YCbCr_to_RGB((y,cb,cr))))
+                pos = (col_base+i, row_base+j)
+                im.putpixel(pos, rgb)
         im.save(output)
 
 
 if __name__ == '__main__':
-    encode('images/test3.bmp', 'images/test3.myjeg')
+    encode('images/test4.bmp', 'images/test4.myjeg', weights=WEIGHTS_DEFAULT)
     print("Finished encoding")
-    decode('images/test3.myjeg', 'images/hi.bmp')
+    decode('images/test4.myjeg', 'images/hi2.bmp', weights=WEIGHTS_DEFAULT)
     print("Finished decoding")
